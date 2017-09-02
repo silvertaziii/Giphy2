d8977b74f0d9cb3a37c7ba266535867a53d95f40
silvertaziii <silvertaziii@yahoo.com>
2017-09-01 23:19:46 -0500

    Add files via upload

---
+++ style.js
@@ -0,0 +1,24 @@
+src="https://github.com/Giphy/GiphyAPI"
+
+
+
+
+
+var queryURL ="https://github.com/Giphy/GiphyAPI#cace4595b2c94015b45e9012a17b33ed"
+	
+
+$.ajax({
+        url: queryURL,
+        method: "GET"
+      }).done(function(response) {
+
+});
+	 $("#select-animal").on("click", function(event) {
+	 	event.preventDefault();
+	 	
+
+
+
+
+
+	 	});
